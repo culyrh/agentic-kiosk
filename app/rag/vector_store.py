eb526800a397de0b7968321a85f097f55e7bebe0
얀 <hyu07776@gmail.com>
2026-03-30 11:43:12 +0900

    메뉴 검색 툴 추가 및 RAG 구조 개선

--- app/rag/vector_store.py
+++ app/rag/vector_store.py
@@ -1,18 +1,21 @@
 
-from langchain_openai import OpenAIEmbeddings
 from langchain_chroma import Chroma
+from app.rag.chroma import get_embedding
 
 
-def create_vector_db(documents):  # upsert 구조 - 중복 방지
-    
-    embedding = OpenAIEmbeddings()
+def create_vector_db_1(documents): # 1회용 벡터 생성
+    # ID 기반 add (collision 시 overwrite)
+
+    embedding = get_embedding()
     
     ids = [str(doc.metadata["id"]) for doc in documents]
     
     db = Chroma(
         persist_directory="data/chroma_db",
-        embedding_function = embedding,
+        embedding_function=embedding,
+        collection_metadata={"hnsw:space": "cosine"},
     )
+    
     db.add_documents(documents=documents, ids=ids)
     
     
--- test.py
+++ test.py
@@ -1,52 +1,45 @@
-# from dotenv import load_dotenv
-# import os
-# from langchain_openai import ChatOpenAI
-# from langchain_core.prompts import ChatPromptTemplate
-# from langchain_core.output_parsers import StrOutputParser
-
-# load_dotenv()
-# api_key = os.getenv("OPENAI_API_KEY")
-
-# llm = ChatOpenAI(model="gpt-4o")
-
-# prompt = ChatPromptTemplate.from_template("주제 {topic}에 대해 어떻게 만들어야 되는지 간략하게 설명해줘")
-
-# parser = StrOutputParser()
-
-# chain = prompt | llm | parser
-
-# response = chain.invoke({"topic": "음성인식 키오스크 ReAct Agent"})
-# print(response)
 
 from app.rag.loader import load_menu
-from app.rag.vector_store import create_vector_db
+from app.rag.vector_store import create_vector_db_1
 from app.rag.chroma import get_chroma_db
 from dotenv import load_dotenv
-import os
+from app.tools.menu_tools import search_menu_logic
 
-from app.tools.search_menu import search_menu 
+
+from app.tools.menu_tools import search_menu 
 
 
 load_dotenv()
-# print(os.getenv("OPENAI_API_KEY"))
 
 doc_menus = load_menu()
-# print(doc_menus)
 
-create_vector_db(doc_menus)
-# da = data.get(include=["embeddings", "documents", "metadatas"])
+
+create_vector_db_1(doc_menus)
+
+
 
 # db = get_chroma_db()
+# query = "치즈 두장 들어가는 햄버거 추천해줘"
+# results = db.similarity_search_with_score(query, k = 10)
+# filtered = [(doc, score) for doc, score in results if score < 0.5]
+# for doc, score in filtered:
+#     print(f"[유사도]: {score}") # 0 에 가까울수록 유사함.
+#     print(doc.page_content)
+#     print("-" * 50)
+    
+    
 
-#### 디버깅용 ####
-# result = db.similarity_search("짭조름하고 달달한 간식 추천해줘", k = 1) 
-# print(result)
-# a = db.get(include=["embeddings", "documents", "metadatas"])
-# print(a["documents"])
 
-
-result = search_menu.invoke({
-    "query": "간단히 먹을 수 있는 사이드 추천해줘",
-    "category": "사이드"
+result = search_menu.invoke({ ## 지금은 수동으로, agent연결 후 자동 ##
+    "query": "바질 들어가는 햄버거 추천해줘",
+    # "exclude": ["우유"],
+    # "category": "버거",
+    # "keyword": "바질",
     })
-print(result)
+
+
+for doc, score in result:
+    print(f"[score: {score}]") # 0 에 가까울수록 유사함.
+    print(doc)
+    print("-" * 50)
+ 
